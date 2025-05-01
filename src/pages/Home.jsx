828c9f0e46d4c8bcfc93dc7666379bee209163ac
Jasur Jovliyev <jasurbekjovliyev725@gmail.com>
2025-05-01 22:10:41 +0500

    Home.jsx faylida o‘zgartirishlar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
 
          <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-5 grid-cols-1 mb-[70px]'>
               { cards.map((card)=>{
-                return <div key={card.title} style={{backgroundColor:card.color}} className='max-w-[380px] h-[280px] md:h-[361px] relative p-5 md:p-[30px] rounded-[10px]'>
+                return <div key={card.title} style={{backgroundColor:card.color}} className='max-w-[320px] mx-auto md:max-w-[380px] h-[280px] md:h-[361px] relative p-5 md:p-[30px] rounded-[10px]'>
                     <div className='flex items-center gap-5 mb-5'>
                         <img className='md:w-[85px] w-14 h-14 md:h-[85px]' src={card.imgurl} alt="img" />
                         <h4 className='md:text-[24px] text-[18px] font-bold open-sans'>{card.title}</h4>
